23dfa907071a83815d55d11e5714454119c1cc24
Harsh <harshv12.hv29@gmail.com>
2022-03-28 18:29:19 +0530

    login activity added

--- app/src/main/java/com/example/etourismuttrakhand/ui/login_screen/LoginScreen.kt
+++ app/src/main/java/com/example/etourismuttrakhand/ui/login_screen/LoginScreen.kt
@@ -1,9 +1,13 @@
 package com.example.etourismuttrakhand.ui.login_screen
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.findNavController
 import com.example.etourismuttrakhand.R
 import com.example.etourismuttrakhand.databinding.ActivityLoginScreenBinding
+import com.example.etourismuttrakhand.ui.MainActivity
+import com.google.android.material.snackbar.Snackbar
 
 class LoginScreen : AppCompatActivity() {
     private lateinit var binding: ActivityLoginScreenBinding
@@ -12,6 +16,28 @@ class LoginScreen : AppCompatActivity() {
         binding = ActivityLoginScreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.loginBtn.isActivated = !binding.firstNameInput.text.toString().isBlank() && !binding.lastNameInput.text.toString().isBlank()
+        binding.loginBtn.isActivated = binding.firstNameInput.text.toString().isNotBlank() &&
+                binding.lastNameInput.text.toString().isNotBlank()
+
+        binding.loginBtn.setOnClickListener {
+            val first = binding.firstNameInput.text.toString()
+            val last = binding.lastNameInput.text.toString()
+            if (first.isNotBlank() && last.isNotBlank()) {
+                val name = "$first/$last"
+                startActivity(
+                    Intent(this,MainActivity::class.java).putExtra("name",name)
+                )
+
+            } else if (first.isBlank() && last.isNotBlank()) {
+
+
+            }
+            else if(first.isNotBlank() && last.isBlank()){
+
+            }else{
+
+            }
+        }
+        }
     }
 }
